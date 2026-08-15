4d848941389fa4c3d55703da8663a81dc0569b8e
Om Sathiya <ommali5428@gmail.com>
2026-08-15 23:05:47 +0530

    index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-
 
 <?php
 
 	include('header.php');
 	
 ?>
+<!DOCTYPE html>
+
+
 <html lang="en">
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
